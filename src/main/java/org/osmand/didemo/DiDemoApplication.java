dd29644b293c7b13e2e8401abdcb3f89e4225e9d
Osmand Das <osmandpdas@gmail.com>
2018-05-17 13:54:43 -0400

    updated

--- src/main/java/org/osmand/didemo/DiDemoApplication.java
+++ src/main/java/org/osmand/didemo/DiDemoApplication.java
@@ -11,8 +11,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
 // by default @SpringBootApplication scans all the packages under its container package
